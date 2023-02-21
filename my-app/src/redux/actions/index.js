ba090319ac68fa530235212393857b5a233f37ba
Giacomo <giax.95@live.it>
2023-02-21 15:33:06 +0100

    Added cases

--- my-app/src/redux/actions/index.js
+++ my-app/src/redux/actions/index.js
@@ -1,34 +1,42 @@
-export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE'
-export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE'
-export const GET_JOBS = 'GET_JOBS'
+export const ADD_TO_FAVOURITE = "ADD_TO_FAVOURITE";
+export const REMOVE_FROM_FAVOURITE = "REMOVE_FROM_FAVOURITE";
+export const GET_JOBS = "GET_JOBS";
 
-const baseEndpoint = 'https://strive-benchmark.herokuapp.com/api/jobs?search='
+export const ADD_TO_FAVOURITE_REQUEST = "ADD_TO_FAVOURITE_REQUEST";
+export const ADD_TO_FAVOURITE_SUCCESS = "ADD_TO_FAVOURITE_SUCCESS";
+export const ADD_TO_FAVOURITE_FAILURE = "ADD_TO_FAVOURITE_FAILURE";
+
+export const REMOVE_FROM_FAVOURITE_REQUEST = "REMOVE_FROM_FAVOURITE_REQUEST";
+export const REMOVE_FROM_FAVOURITE_SUCCESS = "REMOVE_FROM_FAVOURITE_SUCCESS";
+export const REMOVE_FROM_FAVOURITE_FAILURE = "REMOVE_FROM_FAVOURITE_FAILURE";
+
+const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?search=";
 
 export const addToFavouriteAction = (company) => ({
   type: ADD_TO_FAVOURITE,
   payload: company,
-})
+});
 
 export const removeFromFavouriteAction = (company) => ({
   type: REMOVE_FROM_FAVOURITE,
   payload: company,
-})
+});
 
 export const getJobsAction = (query) => {
   return async (dispatch) => {
     try {
-      const response = await fetch(baseEndpoint + query + '&limit=20')
+      const response = await fetch(baseEndpoint + query + "&limit=20");
       if (response.ok) {
-        const { data } = await response.json()
+        const { data } = await response.json();
         dispatch({
           type: GET_JOBS,
           payload: data,
-        })
+        });
       } else {
-        alert('Error fetching results')
+        alert("Error fetching results");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-}
+  };
+};
--- my-app/src/redux/reducers/favourite.js
+++ my-app/src/redux/reducers/favourite.js
@@ -1,24 +1,51 @@
-import { ADD_TO_FAVOURITE, REMOVE_FROM_FAVOURITE } from '../actions'
+import {
+  ADD_TO_FAVOURITE_REQUEST,
+  ADD_TO_FAVOURITE_SUCCESS,
+  ADD_TO_FAVOURITE_FAILURE,
+  REMOVE_FROM_FAVOURITE_REQUEST,
+  REMOVE_FROM_FAVOURITE_SUCCESS,
+  REMOVE_FROM_FAVOURITE_FAILURE,
+} from "../actions";
 
 const initialState = {
   list: [],
-}
+  loading: false,
+  error: null,
+};
 
 const favouriteReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_TO_FAVOURITE:
+    case ADD_TO_FAVOURITE_REQUEST:
+    case REMOVE_FROM_FAVOURITE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case ADD_TO_FAVOURITE_SUCCESS:
       return {
         ...state,
         list: [...state.list, action.payload],
-      }
-    case REMOVE_FROM_FAVOURITE:
+        loading: false,
+        error: null,
+      };
+    case REMOVE_FROM_FAVOURITE_SUCCESS:
       return {
         ...state,
         list: state.list.filter((fav) => fav !== action.payload),
-      }
+        loading: false,
+        error: null,
+      };
+    case ADD_TO_FAVOURITE_FAILURE:
+    case REMOVE_FROM_FAVOURITE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default favouriteReducer
+export default favouriteReducer;
